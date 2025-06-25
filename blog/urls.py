88da24303ec429ccca172d88c4abcf0f40da89b4
mateo492 <tu-email-de-github@gmail.com>
2025-06-25 18:49:00 -0300

    Cambio ruta para usar /evaluacion2

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.lista_public, name=' lista_public'),
+    path('evaluacion2', views.lista_public, name='lista_public'),
 ]
